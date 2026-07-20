b1e23a92932900c03b2470c2a202f81ced055c7f
Whooper123233 <veronica.wong1223@gmail.com>
2026-07-20 14:48:58 +0100

    gonna try to do saving system next

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,33 +9,56 @@ public class LevelManager : MonoBehaviour
     public static LevelManager Instance;
     [SerializeField] private GameObject loaderCanvas;
     [SerializeField] private Image progressBar;
+    [SerializeField] private float minLoadTime = 1.5f;   
+    [SerializeField] private float fillSpeed = 3f;         
+
     void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
     }
 
-    public async void LoadScene(string sceneName)
+    public void LoadScene(string sceneName)
     {
+        StartCoroutine(LoadSceneRoutine(sceneName));
+    }
+
+    private IEnumerator LoadSceneRoutine(string sceneName)
+    {
+        loaderCanvas.SetActive(true);
+        progressBar.fillAmount = 0f;
+
+        float elapsed = 0f;
+        float displayedProgress = 0f;
+
         var scene = SceneManager.LoadSceneAsync(sceneName);
         scene.allowSceneActivation = false;
 
-        loaderCanvas.SetActive(true);
-
-        do
+        while (elapsed < minLoadTime || scene.progress < 0.9f)
         {
-            await Task.Delay(1000);
+            elapsed += Time.deltaTime;
 
-            progressBar.fillAmount = scene.progress;
-        } while (scene.progress < 0.9f);
+            float targetProgress = Mathf.Clamp01(scene.progress / 0.9f);
+            displayedProgress = Mathf.MoveTowards(displayedProgress, targetProgress, fillSpeed * Time.deltaTime);
+            progressBar.fillAmount = displayedProgress;
+
+            yield return null;
+        }
+
+        while (displayedProgress < 1f)
+        {
+            displayedProgress = Mathf.MoveTowards(displayedProgress, 1f, fillSpeed * Time.deltaTime);
+            progressBar.fillAmount = displayedProgress;
+            yield return null;
+        }
 
         scene.allowSceneActivation = true;
+
+        yield return null;
+
         loaderCanvas.SetActive(false);
-
-
-        ;
     }
 }
